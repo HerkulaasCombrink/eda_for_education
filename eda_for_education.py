5c85edffc0c9cc838a1a9ed6157f707e267f93e2
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2022-03-27 16:25:43 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -1,4 +1,3 @@
 import streamlit as st
 st.markdown('_Welcome to the exploratory data analysis application. Built into this application is a pipeline that can be applied to your context_')
 df = st.file_uploader('Upload a CSV')
-df
