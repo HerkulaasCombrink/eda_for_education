3dfcb42567ccd5821915a9d37fce7b192c84318c
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2022-03-27 16:36:44 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -1,9 +1,21 @@
 import streamlit as st
+import pandas as pd
+import sweetviz as sv
+import numpy as np
+#initial instructions
 st.markdown('**Welcome to the exploratory data analysis application.**')
 st.markdown('_Built into this application is a pipeline that can be applied to your context_')
 st.markdown('**Start by loading a CSV file into the App. Please ensure that you upload a CSV file, with one sheet. If you have multiple spreadsheets, consolidate them into one**')
 
 st.markdown('_If you are unsure how to do this, please go to the following link https://www.computerhope.com/issues/ch001356.htm_')
-
+#loading the first part of the data
 
 df = st.file_uploader('Upload a CSV')
+df.head()
+summary = edacsv.describe()
+summary = summary.transpose()
+summary
+
+df_corr = hitMap(df,20,0.95,0.15)
+f,ax = plt. subplots(figsize = (10,10))
+sns.heatmap(df_corr, annot = False,cmap= 'coolwarm' )
