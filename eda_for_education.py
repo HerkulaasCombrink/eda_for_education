e0712edceeaebff4b56e1cbb6ca2e72f1b7955ed
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2022-03-27 16:28:46 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.markdown('**Welcome to the exploratory data analysis application.**')
 st.markdown('_Built into this application is a pipeline that can be applied to your context_')
-
+st.markdown('**Start by loading a CSV file into the App. Please ensure that you upload a CSV file, with one sheet. If you have multiple spreadsheets, consolidate them into one**')
 
 df = st.file_uploader('Upload a CSV')
