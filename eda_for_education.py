df0649e47bb1549c63752bbbcbac83b24b7997af
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2022-03-27 16:27:36 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.markdown('_**Welcome to the exploratory data analysis application.**')
+st.markdown('**Welcome to the exploratory data analysis application.**')
 st.markdown('_Built into this application is a pipeline that can be applied to your context_')
 
 
