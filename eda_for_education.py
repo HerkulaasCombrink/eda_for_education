eea5f6db3501a1cd59caed1a50d065291c104e6f
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2022-03-27 16:32:10 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -3,4 +3,7 @@ st.markdown('**Welcome to the exploratory data analysis application.**')
 st.markdown('_Built into this application is a pipeline that can be applied to your context_')
 st.markdown('**Start by loading a CSV file into the App. Please ensure that you upload a CSV file, with one sheet. If you have multiple spreadsheets, consolidate them into one**')
 
+st.markdown('_If you are unsure how to do this, please go to the following link https://www.computerhope.com/issues/ch001356.htm_')
+
+
 df = st.file_uploader('Upload a CSV')
