55d883fad0c7e4737e36b67c781155f8d15701c3
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2022-03-27 16:14:07 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-if st.button('Wish Good morning'):
+st.button('Wish Good morning')
 
-    st.write('Good Morning') #displayed when the button is clicked
-
-else:
-
-    st.write('Have a great day') #displayed when the button is unclicked
-
+   
