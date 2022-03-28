9b4cee223eda8dc4f897baee8bc9228ecb8035bc
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2022-03-28 09:46:51 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -55,7 +55,7 @@ df_copy = mapColName_Int(df)
 
 st.markdown('**Insert Heatmap Here**')
 df_corr = hitMap(df_copy,20,0.95,0.15)
- 
+st.write(df_corr)
 #sweet_report = sv.analyze(df)
 #Sweet_report.show_html('sweetviz_report.html')
 
