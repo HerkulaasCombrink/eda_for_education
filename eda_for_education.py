0599ca959be4f54d620af8563adcf12c21b462ae
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2022-03-28 09:35:18 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -16,9 +16,9 @@ st.markdown('_If you are unsure how to do this, please go to the following link 
 
 #loading the first part of the data
 
-df = st.file_uploader('Upload a CSV')
-df2 = pd.DataFrame(
-    np.df,
+#df = st.file_uploader('Upload a CSV')
+df = pd.DataFrame(
+    st.file_uploader('Upload a CSV'),
     columns=('col %d' % i for i in range(5)))
 
 st.table(df)
