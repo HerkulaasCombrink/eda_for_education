c6fcb15540857c12ad13f3ed1fb547dcf8d9acad
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2022-03-28 14:55:16 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -142,5 +142,5 @@ st.markdown('***In the table before, the three variable pairs with the highest c
 st.write(df_report)
 
 analysis = sv.analyze(df)
-analysis.show_html(open_browser=False, layout='vertical', scale=1.0)
-components.iframe(width=1100, height=1200, scrolling=True)
+analysis.show_html(open_browser=True)
+components.iframe()
