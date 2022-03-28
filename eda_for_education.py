86a41856784ec90c54f67898b8d4705c16bdfd79
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2022-03-28 09:38:46 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -20,7 +20,7 @@ st.markdown('_If you are unsure how to do this, please go to the following link 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  st.write(dataframe)
+  st.write(df)
 
 
 
