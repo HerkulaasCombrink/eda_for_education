ed05ef4074c8b2a49529c3292a85a086bf57c02e
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2024-09-18 08:50:37 +0200

    Update eda_for_education.py

--- eda_for_education.py
+++ eda_for_education.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import sweetviz as sv
+#import sweetviz as sv
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
